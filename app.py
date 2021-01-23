2195c69d7dfba862b2c057b579c2c0c829bb6eae
Scott Nixon <scott@happyherbivore.com>
2021-01-23 11:02:53 -0800

    Test cli cmd

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
+import os
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    stream = os.popen('echo Returned output')
+    output = stream.read()
+
+    return f'Hello, World!\n\n {output}'
